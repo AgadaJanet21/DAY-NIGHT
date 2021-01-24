b7bb9b2e2f418658caabdc659578796d26bfc045
AgadaJanet21 <agadaejanet@gmail.com>
2021-01-24 20:46:36 +0100

    my first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var dayBtn = document.querySelector("#day");
+var nightBtn = document.querySelector("#night");
+var displayMessage = document.querySelector("#message");
+var displayMoon = document.querySelector(".moon");
+
+
+dayBtn.addEventListener("click", function(){
+    nightBtn.classList.remove("selected");
+    dayBtn.classList.add("selected");
+    displayMessage.textContent = "HEY! GOOD DAY ! Wishing you an amzaing day ahead !";
+    displayMoon.style.display ="none"
+});
+
+nightBtn.addEventListener("click", function(){
+    dayBtn.classList.remove("selected");
+    nightBtn.classList.add("selected");
+    
+    
+});
+
+
+
