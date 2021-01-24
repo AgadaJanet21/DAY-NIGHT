bd213fb266393d6e99fe23ad75f53bf3cff9a93e
AgadaJanet21 <agadaejanet@gmail.com>
2021-01-24 23:27:22 +0100

    my third commit

--- script.js
+++ script.js
@@ -2,21 +2,37 @@ var dayBtn = document.querySelector("#day");
 var nightBtn = document.querySelector("#night");
 var displayMessage = document.querySelector("#message");
 var displayMoon = document.querySelector(".moon");
+var displayDay = document.querySelector("#days");
+var selected = document.querySelector(".selected");
+document.getElementById("days").style.paddingTop = "20px";
+document.getElementById("days").style.lineHeight = "2";
 
+
+if(selected === selected) {
+    displayMessage.style.display ="block";
+    displayMoon.style.display ="block";
+    displayDay.style.display ="none";
+}
+
+
+nightBtn.addEventListener("click", function(){
+    nightBtn.classList.add("selected");
+    dayBtn.classList.remove("selected");
+    displayMoon.style.display ="block";
+    displayMessage.style.display ="block";
+    displayDay.style.display ="none";
+});
 
 dayBtn.addEventListener("click", function(){
     nightBtn.classList.remove("selected");
     dayBtn.classList.add("selected");
-    displayMessage.textContent = "HEY! GOOD DAY ! Wishing you an amzaing day ahead !";
-    displayMoon.style.display ="none"
+    
+    displayMoon.style.display ="none";
+    displayDay.style.display = "block";
+    displayMessage.style.display ="none";
 });
 
-nightBtn.addEventListener("click", function(){
-    dayBtn.classList.remove("selected");
-    nightBtn.classList.add("selected");
-    displayMessage.textContent = displayMessage;
-    
-});
+
 
 
 
